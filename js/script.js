2f202851cab0c9f710a83ea859b8bf81ae4a50a0
jamesbrainard <130089555+jamesbrainard@users.noreply.github.com>
2024-02-29 10:07:35 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const animatedMap = document.getElementById("animatedMap");
+    const mapContainer = document.getElementById("mapContainer");
     const speedSlider = document.getElementById("speedSlider");
 
-    const years = Array.from({ length: 27 }, (_, index) => 1992 + index);
+    const years = Array.from({ length: 27 }, (_, index) => 1992 + index); 
 
     let currentYearIndex = 0;
     let animationSpeed = 1000; 
 
     function updateMap() {
         const currentYear = years[currentYearIndex];
-        animatedMap.style.backgroundImage = `url('png/LightPollution_${currentYear}.png')`;
+        animatedMap.src = `png/LightPollution_${currentYear}.png`;
     }
 
     function animateMap() {
@@ -18,9 +19,12 @@ document.addEventListener("DOMContentLoaded", function () {
         setTimeout(animateMap, animationSpeed);
     }
 
+    animatedMap.style.width = "100%";
+    animatedMap.style.height = "auto";
+
     animateMap();
 
     speedSlider.addEventListener("input", function () {
-        animationSpeed = 2000 / speedSlider.value;
+        animationSpeed = 2000 / speedSlider.value; // Adjust the speed based on the slider value
     });
 });
